ca93b817f3f8c35ed44e7d5027bbf496c1e3517a
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-08-02 19:51:07 -0700

    satisfy jest warning

--- test/src/Path.test.tsx
+++ test/src/Path.test.tsx
@@ -23,15 +23,17 @@ test("Path should render correctly", () => {
 
 test("Path should render correctly without an end hex", () => {
   const { container } = render(
-    <Layout
-      className={"test2"}
-      size={{ x: 6, y: 6 }}
-      flat={false}
-      spacing={1.1}
-      origin={{ x: 0, y: 0 }}
-    >
-      <Path start={{ q: 1, r: 1, s: -1 }} />
-    </Layout>,
+    <svg>
+      <Layout
+        className={"test2"}
+        size={{ x: 6, y: 6 }}
+        flat={false}
+        spacing={1.1}
+        origin={{ x: 0, y: 0 }}
+      >
+        <Path start={{ q: 1, r: 1, s: -1 }} />
+      </Layout>
+    </svg>,
   )
   expect(container.firstChild).toMatchSnapshot()
 })
