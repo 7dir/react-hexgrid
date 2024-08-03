d199bfeccc1c030faf98d112283fdef310dd6b5c
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-08-02 19:35:42 -0700

    ?

--- src/stories/way-of-life/way-of-life.stories.tsx
+++ src/stories/way-of-life/way-of-life.stories.tsx
@@ -130,7 +130,7 @@ const Template: StoryFn<typeof Hexagon> = (args, { argTypes }) => {
         <Layout size={size} flat={true} spacing={1}>
           {Object.keys(hexagons)
             .map((v) => hexagons[v])
-            .map((hex, _) => (
+            .map((hex, i) => (
               <Hexagon
                 key={i}
                 q={hex.q}
