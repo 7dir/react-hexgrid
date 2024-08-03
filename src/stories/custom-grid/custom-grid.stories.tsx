6280ea290f964e36eb8b38effad4a4eda3d447b3
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-08-03 15:52:44 -0700

    pattern isn't working, thinking about refactor

--- src/stories/custom-grid/custom-grid.stories.tsx
+++ src/stories/custom-grid/custom-grid.stories.tsx
@@ -10,7 +10,10 @@ import {
   Pattern,
   Hex,
 } from "../.."
-import { css } from "@emotion/react"
+
+import type { HexagonProps } from "../../Hexagon/Hexagon"
+import type { TextProps } from "../../Hexagon/Text"
+
 import styled from "@emotion/styled"
 
 export default {
@@ -23,39 +26,56 @@ const hexagonSize = { x: 10, y: 10 }
 const moreHexas = GridGenerator.parallelogram(-2, 2, -2, 2)
 
 /*
-const Hexagon = styled(H)`
-  stroke: #7be3f6;
-  stroke-width: 0.2;
-  transition: fill-opacity 0.5s;
-  &:hover {
-    fill: #7be3f6;
-    fill-opacity: 0.7;
-  }
-`
-
 USE THIS:
 https://refine.dev/blog/react-svg/#adding-interactivity
 */
 
-const Hexagon = (
-  <H
-    q={0}
-    r={0}
-    s={0}
-    cellStyle={{
-      stroke: "#7be3f6",
-      strokeWidth: "0.2",
-      transition: "fill-opacity 0.5s",
-    }}
-  />
-)
+function Hexagon(props: HexagonProps): React.JSX.Element {
+  console.log(props.fill)
+  return (
+    <H
+      q={props.q}
+      r={props.r}
+      s={props.s}
+      fill={props.fill}
+      strokeWidth="0.2"
+      stroke="#7be3f6"
+      cellStyle={{
+        fill: "rgba(123,227,246, 0.7)",
+        transition: "fill-opacity 0.5s",
+      }}
+      onMouseEnter={(e) =>
+        (e.target as SVGElement).setAttribute("fill-opacity", "0.1")
+      }
+      onMouseLeave={(e) =>
+        (e.target as SVGElement).setAttribute("fill-opacity", "1")
+      }
+    >
+      {props.children}
+    </H>
+  )
+}
 
+function Text(props: TextProps) {
+  return (
+    <T
+      fontSize="0.22em"
+      fill="white"
+      fillOpacity="0.7"
+      style={{ transition: "fill-opacity 0.5s" }}
+    >
+      {props.children}
+    </T>
+  )
+}
+/*
 const Text = styled(T)`
   font-size: 0.22em;
   fill: white;
   fill-opacity: 0.7;
   transition: fill-opacity 0.5s;
 `
+*/
 
 const Path = styled(P)`
   fill: none;
@@ -89,10 +109,6 @@ const Template: StoryFn<typeof H> = (args, { argTypes }) => {
           flat={true}
           spacing={1.1}
           origin={{ x: 0, y: 0 }}
-          css={css`
-            fill: #4499a9;
-            fill-opacity: 0.6;
-          `}
         >
           <Hexagon q={0} r={0} s={0} />
           {/* Using pattern (defined below) to fill the hexagon */}
@@ -109,18 +125,12 @@ const Template: StoryFn<typeof H> = (args, { argTypes }) => {
             <Text>-1, 1, 0</Text>
           </Hexagon>
           <Hexagon q={-1} r={0} s={1} />
+
           <Hexagon q={-2} r={0} s={1} />
           <Path start={new Hex(0, 0, 0)} end={new Hex(-2, 0, 1)} />
         </Layout>
         {/* Additional small grid, hexagons generated with generator */}
-        <Layout
-          css={css`
-            fill: #4499a9;
-            fill-opacity: 0.6;
-          `}
-          size={{ x: 2, y: 2 }}
-          origin={{ x: 50, y: -30 }}
-        >
+        <Layout size={{ x: 2, y: 2 }} origin={{ x: 50, y: -30 }}>
           {moreHexas.map((hex, i) => (
             <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />
           ))}
@@ -137,12 +147,7 @@ const Template: StoryFn<typeof H> = (args, { argTypes }) => {
           //   link="http://lorempixel.com/400/400/cats/2/"
           size={hexagonSize}
         />
-        <g
-          css={css`
-            fill: #4499a9;
-            fill-opacity: 0.6;
-          `}
-        >
+        <g fillOpacity="0.6" fill={"#4499a9"}>
           <circle cx="50" cy="0" r="10" />
           <circle cx="50" cy="10" r="8" />
           <circle cx="45" cy="20" r="6" />
